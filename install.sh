fb03c01949b6be30f1d7669af2bb9d7e53a91e81
Xijin Zhang <mfs6174@gmail.com>
2012-02-08 21:11:23 +0800

    提供安装脚本

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+extension_name="CE2SW@mfs6174.tk"
+
+if [ $UID -eq 0 ];then
+	DEST="/usr/share/gnome-shell/extensions/$extension_name"
+else
+	DEST=$HOME/.local/share/gnome-shell/extensions/$extension_name/
+fi
+mkdir -p $DEST || exit 1
+cp extension.js  metadata.json stylesheet.css LICENSE "$DEST" || exit 1
+echo "Install successfull!"
+echo "Enable it with gnome-tweak-tool"
